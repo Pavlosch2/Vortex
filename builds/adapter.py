514b9a84e9994566ca9835e47560a8a5cac56d5e
Pavlo <pavlosirba974@gmail.com>
2026-04-26 18:59:16 +0300

    google OAuth fixes x5

--- builds/adapter.py
+++ builds/adapter.py
@@ -5,6 +5,20 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def is_auto_signup_allowed(self, request, sociallogin):
         return True
 
+    def pre_social_login(self, request, sociallogin):
+        if sociallogin.is_existing:
+            return
+        if not sociallogin.user.username:
+            email = sociallogin.account.extra_data.get('email', '')
+            base = re.sub(r'[^a-zA-Z0-9]', '', email.split('@')[0])[:20] or 'user'
+            from django.contrib.auth.models import User
+            username = base
+            counter = 1
+            while User.objects.filter(username=username).exists():
+                username = f"{base}{counter}"
+                counter += 1
+            sociallogin.user.username = username
+
     def populate_user(self, request, sociallogin, data):
         user = super().populate_user(request, sociallogin, data)
         if not user.username:
