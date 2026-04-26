62ab94b7367637b5272b146fe40be30363ae5a79
Pavlo <pavlosirba974@gmail.com>
2026-04-27 00:42:12 +0300

    settings.py fixes

--- core/settings.py
+++ core/settings.py
@@ -189,7 +189,8 @@ EMAIL_BACKEND = os.getenv(
 )
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "noreply@vortexpro.com")
